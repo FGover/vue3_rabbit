b755d432baf214576ad85e59bd5cc21568b21afc
fghwzw <2924074463@qq.com>
2024-01-25 17:22:45 +0800

    'banner轮播图渲染'

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,8 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElCarousel: typeof import('element-plus/es')['ElCarousel']
+    ElCarouselItem: typeof import('element-plus/es')['ElCarouselItem']
     ElPopconfirm: typeof import('element-plus/es')['ElPopconfirm']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
--- src/views/Home/components/HomeBanner.vue
+++ src/views/Home/components/HomeBanner.vue
@@ -1,3 +1,38 @@
 <template>
-  Banner
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="" />
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
+
+<script setup name="HomeBanner">
+import { getBannerAPI } from '@/stores/home'
+import { ref, onMounted } from 'vue'
+const bannerList = ref([])
+const getBanner = async () => {
+  const {result: res} = await getBannerAPI()
+  bannerList.value = res
+}
+
+onMounted(() => {
+  getBanner()
+})
+</script>
+
+<style lang="scss" scoped>
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  z-index: 98;
+  position: absolute;
+  left: 0;
+  top: 0;
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
+</style>
